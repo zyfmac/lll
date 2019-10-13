a33a664521ad20949ece0041f7cdbde61a125635
zyfmac <zyfmac@outlook.com>
2019-10-12 23:26:13 -0400

    第二次提交，刚刚改了。。

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("wold");
-        System.out.println("Copy");
+        System.out.println("....");
+
     }
 }
